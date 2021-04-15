4d9c63b8abee44a9522189936ef4d2d8af4548af
NatasaC <natasa.cirstea@yahoo.com>
2021-04-15 12:04:24 +0300

    added navigation drawer

--- Community-Basket /app/src/main/java/com/example/community_basket/MainActivity.kt
+++ Community-Basket /app/src/main/java/com/example/community_basket/MainActivity.kt
@@ -2,13 +2,20 @@ package com.example.community_basket
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.community_basket.SecondActivity
 import com.example.community_basket.R
 import com.example.community_basket.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,21 +23,46 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        toggle = ActionBarDrawerToggle(this, binding.drawerLayout, R.string.open, R.string.close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
 
-        binding.btActivitySecond.setOnClickListener {
-            gotoSecondApp()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }
+
+                R.id.products -> {
+                    val intent = Intent(this, SecondActivity::class.java)
+                    startActivity(intent)
+                }
+
+                R.id.login -> Toast.makeText(applicationContext, "login", Toast.LENGTH_SHORT).show()
+            }
+
+            true
         }
 
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .setReorderingAllowed(true)
-                .add(R.id.fragment_container, FirstFragment::class.java, null)
-                .commit()
-        }
+//        binding.btActivitySecond.setOnClickListener {
+//            gotoSecondApp()
+//        }
+//
+//        if (savedInstanceState == null) {
+//            supportFragmentManager.beginTransaction()
+//                .setReorderingAllowed(true)
+//                .add(R.id.fragment_container, FirstFragment::class.java, null)
+//                .commit()
+//        }
     }
 
-    private fun gotoSecondApp() {
-        val intent = Intent(this, SecondActivity::class.java)
-        startActivity(intent)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- Community-Basket /app/src/main/java/com/example/community_basket/SecondActivity.kt
+++ Community-Basket /app/src/main/java/com/example/community_basket/SecondActivity.kt
@@ -1,6 +1,10 @@
 package com.example.community_basket
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -8,6 +12,7 @@ import com.example.community_basket.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySecondBinding
+    lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,13 +20,44 @@ class SecondActivity : AppCompatActivity() {
         binding = ActivitySecondBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setupActionBarWithNavController(findNavController(R.id.list_fragment))
+        toggle = ActionBarDrawerToggle(this, binding.drawerLayout, R.string.open, R.string.close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }
+
+                R.id.products -> {
+                    val intent = Intent(this, SecondActivity::class.java)
+                    startActivity(intent)
+                }
+
+                R.id.login -> Toast.makeText(applicationContext, "login", Toast.LENGTH_SHORT).show()
+            }
+
+            true
+        }
+
+//        setupActionBarWithNavController(findNavController(R.id.list_fragment))
 
     }
+//
+//    // back arrow add product
+//    override fun onSupportNavigateUp(): Boolean {
+//        val navController = findNavController(R.id.list_fragment)
+//        return navController.navigateUp() || super.onSupportNavigateUp()
+//    }
 
-    // back arrow add product
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.list_fragment)
-        return navController.navigateUp() || super.onSupportNavigateUp()
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
